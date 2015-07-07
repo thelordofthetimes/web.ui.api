6013c3aa51a1c14135eaa570d58d064468666159
Bui Tuan Anh <anhbt@dev.cdna.net>
2015-07-07 19:08:18 +0700

    save

--- src/script/directive/dropdown.js
+++ src/script/directive/dropdown.js
@@ -1,5 +1,19 @@
 (function(angular) {
     angular.module('web.ui.api').
+    /**
+     * provide background api for dropdown component.
+     * use:
+     *  - <tag web-dropdown="" ng-model="dropdownModel"></tag>
+     *  - <web-dropdown ng-model="dropdownModel"></web-dropdown>
+     * description:
+     *  - component: dropdown-toggle, dropdown-menu.
+     *  - click to dropdown-toggle: toggle ngModel.
+     *  - click out of dropdown: change ngModel to 'false'
+     * api:
+     *  ngModel {boolean}:
+     *   - true: dropdown is show
+     *   - false: dropdown is hide
+     */
         directive('webDropdown', function() {
             // define directive
             return {
