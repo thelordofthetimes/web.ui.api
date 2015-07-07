7a534f7faea999157d726a076e231d815442ba10
Bui Tuan Anh <anhbt@dev.cdna.net>
2015-07-07 10:40:45 +0700

    save

--- src/script/directive/date-picker.js
+++ src/script/directive/date-picker.js
@@ -1,6 +1,22 @@
 (function(angular) {
     angular.module('web.ui.api').
         directive('webDatePicker', [function() {
+            /**
+             * link function
+             * @param scope
+             * @param elem
+             * @param atrrs
+             */
+            function link(scope, elem, atrrs) {
 
+            }
+
+            // define directive
+            return {
+                restrict: 'E',
+                templateUrl: 'web.ui.template/date-picker.html',
+                scope: {},
+                link: link
+            }
         }]);
 })(angular);
--- src/script/directive/dropdown.js
+++ src/script/directive/dropdown.js
@@ -1,6 +1,22 @@
 (function(angular) {
     angular.module('web.ui.api').
         directive('webDropdown', [function() {
+            /**
+             * link function
+             * @param scope
+             * @param elem
+             * @param attrs
+             */
+            function link(scope, elem, attrs) {
 
+            }
+
+            // define directive
+            return {
+                restrict: 'E',
+                templateUrl: 'web.ui.template/dropdown.html',
+                scope: {},
+                link: link
+            }
         }]);
 })(angular);
