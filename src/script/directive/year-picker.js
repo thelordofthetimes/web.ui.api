ad3322ce949c5be49845c96d8ddec1f599227fd3
Bui Tuan Anh <anhbt@dev.cdna.net>
2015-07-09 15:39:49 +0700

    save

--- src/script/directive/year-picker.js
+++ src/script/directive/year-picker.js
@@ -1,10 +1,49 @@
-(function(angular) {
+(function (angular) {
     angular.module('web.ui.api').
-        controller('WebYearPickerCtrl', ['$scope', function($scope) {
-            $scope.years = [];
+        constant('yearPickerConst', {
+            PAGE_SIZE: 12
+        }).
+        service('web.ui.api.yearPaging', [function () {
+            /**
+             * get page of year
+             * @param pageSize {int}
+             * @param pageIndex {int}
+             * @returns {Array<int>}
+             */
+            function yearPaging(pageSize, pageIndex) {
+                var startYear = (pageIndex - 1) * pageSize;
+                var years = [];
+                for (var index = 0; index < pageSize; index++) {
+                    years.push(startYear + index);
+                }
+                return years;
+            }
 
+            // define service
+            return yearPaging;
         }]).
-        directive('webYearPicker', [function() {
+        controller('WebYearPickerCtrl', ['$scope', '$injector', function ($scope, $injector) {
+            var yearPickerConst = $injector.get('yearPickerConst');
+            var $filter = $injector.get('$filter');
+            var yearPaging = $injector.get('web.ui.api.yearPaging');
+
+            $scope.years = [];
+            $scope.pageSize = yearPickerConst.PAGE_SIZE;
+            $scope.pageIndex = 1;
+
+            $scope.updateModel = function (value) {
+                $scope.ngModel = value;
+            };
+
+            $scope.$watch('[pageSize, pageIndex]', function (nv) {
+                $scope.years.length = 0;
+                var rows = $filter('chunk')(yearPaging(nv[0], nv[1]), 4);
+                rows.forEach(function (row) {
+                    $scope.years.push(row);
+                });
+            }, true);
+        }]).
+        directive('webYearPicker', [function () {
             return {
                 restrict: 'AE',
                 templateUrl: 'web.ui.template/year-picker.html',
@@ -12,9 +51,11 @@
                     ngModel: '=?'
                 },
                 controller: 'WebYearPickerCtrl',
-                link: function(scope, elem, attrs) {
-                    if (!scope.ngModel) {}
-                    scope.ngModel = 0;
+                link: function (scope, elem, attrs) {
+                    if (!scope.ngModel) {
+                        scope.ngModel = 0;
+                    }
+                    scope.model = angular.copy(scope.ngModel);
                 }
             }
         }])
