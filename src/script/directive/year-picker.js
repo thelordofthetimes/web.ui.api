da132eccc546a8dba24a0570a706bdc9854e017d
Bui Tuan Anh <anhbt@dev.cdna.net>
2015-07-09 22:25:23 +0700

    save

--- src/script/directive/year-picker.js
+++ src/script/directive/year-picker.js
@@ -28,10 +28,11 @@
             var yearPickerConst = $injector.get('yearPickerConst');
             var _pageIndex;
             var _pageSize;
+            var _model = angular.copy($scope.ngModel);
 
-            // initialize value
-            $scope.years = [];
-
+            /**
+             * {int} get/set pageSize
+             */
             Object.defineProperty($scope, 'pageSize', {
                 get: function () {
                     return _pageSize;
@@ -42,15 +43,10 @@
                     }
                 }
             });
-            if (!$scope.pageSize) {
-                $scope.pageSize = yearPickerConst.PAGE_SIZE;
-            }
 
-
-            if (!$scope.pageIndex) {
-                $scope.pageIndex = 1;
-            }
-
+            /**
+             * {int} get/set pageIndex
+             */
             Object.defineProperty($scope, 'pageIndex', {
                 get: function () {
                     return _pageIndex;
@@ -62,32 +58,59 @@
                 }
             });
 
-            $scope.applyModel = function (value) {
-                console.log(value);
-                $scope.ngModel = value;
-            };
+            /**
+             * {int} get/set model
+             */
+            Object.defineProperty($scope, 'model', {
+                get: function () {
+                    return _model;
+                },
+                set: function (value) {
+                    _model = value;
+                }
+            });
 
+
+            /**
+             * apply model to ngModel
+             * @param value {int}
+             */
+            function applyModel(value) {
+                $scope.ngModel = value;
+            }
+
+            /**
+             * get now page index
+             * @param pageSize {int}
+             * @returns {number} now page index
+             */
+            function nowPageIndex(pageSize) {
+                var date = new Date();
+                var year = date.getFullYear();
+                return Math.ceil(year / pageSize);
+            }
+
+            // observer pageSize, pageIndex to regenerate year page
             $scope.$watch('[pageSize, pageIndex]', function (nv) {
                 var pageIndex = nv[1];
                 if (pageIndex == -1) {
-                    var date = new Date();
-                    var year = date.getFullYear();
-                    pageIndex = Math.ceil(year / $scope.pageSize);
+                    pageIndex = nowPageIndex($scope.pageSize);
                 }
 
                 $scope.pageIndex = pageIndex;
                 $scope.years = yearPaging(nv[0], pageIndex);
             }, true);
 
-            var model = angular.copy($scope.ngModel);
-            Object.defineProperty($scope, 'model', {
-                get: function () {
-                    return model;
-                },
-                set: function (value) {
-                    model = value;
-                }
-            });
+            // public method to scope
+            $scope.applyModel = applyModel;
+
+            // initialize
+            if (!$scope.pageSize) {
+                $scope.pageSize = yearPickerConst.PAGE_SIZE;
+            }
+            if (!$scope.pageIndex) {
+                $scope.pageIndex = 1;
+            }
         }]).
         directive('webYearPicker', [function () {
             return {
